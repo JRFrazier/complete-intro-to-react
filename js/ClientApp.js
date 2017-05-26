b15e8e3d4c2291eb403196e772ede08710456892
JR <jfrazier@tiggee.com>
2017-05-26 12:43:56 -0400

    added lint rule to webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import '../public/normalize.css'
 import '../public/style.css'
 
-const App = React.createClass ({
+const App = React.createClass({
   render () {
     return (
       <div className='app'>
